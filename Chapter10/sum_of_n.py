7f6c40216c9ec8194e10c47aa86cb64a648660a5
NHlaing <nadarwinhlaing2001@gmail.com>
2020-08-12 13:47:00 -0700

    12.08.2020

---
+++ Chapter10/sum_of_n.py
@@ -0,0 +1,6 @@
+def sum_of_n(n):
+    the_sum = 0
+    for i in range(1,n+1):
+        the_sum = the_sum + i
+    return the_sum
+print(sum_of_n(10))
---
+++ Chapter9/bfs(node_tree).py
@@ -0,0 +1,42 @@
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.child = []
+    def __repr__(self):
+        return "Value is " + self.value
+    def insert_child(self,node):
+        self.child.append(node)
+    def get_child(self):
+        return self.child
+    def bfs(self):
+        thislevel = [self]
+        while thislevel:
+            nextlevel = []
+            level = []
+            for n in thislevel:
+                level.append(n.value)
+                if len(n.child) > 0:
+                    nextlevel = nextlevel + n.child
+                    if len(level) > 0:
+                        print(",".join(level))
+                        thislevel = nextlevel
+root = Node("A")
+b = Node("B")
+c = Node("C")
+d = Node("D")
+root.insert_child(b)
+root.insert_child(c)
+root.insert_child(d)
+e = Node("E")
+f = Node("F")
+g = Node("G")
+b.insert_child(e)
+b.insert_child(f)
+b.insert_child(g)
+h = Node("H")
+i = Node("I")
+c.insert_child(h)
+c.insert_child(i)
+j = Node("J")
+d.insert_child(j)
+root.bfs()
